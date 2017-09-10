ebbfec99e727424b7ed548f608b14bf2bdc63440
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-10 15:33:19 -0400

    more practice, stopped at 3:33pm, will resume in a bit

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-function b() {
-  var myVar;
-  console.log(myVar);
-}
 function a() {
-  var myVar = 2;
-  console.log(myVar);
+  function b() {
+    console.log(myVar);
+  }
+  
   b();
 }
-
 var myVar = 1;
-console.log(myVar);
 a();
-console.log(myVar);
