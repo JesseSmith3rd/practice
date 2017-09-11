b377f5c53b2b9a9dd297b47a92af88b0b7b3ce63
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-11 17:39:17 -0400

    will finish later, been doing this all day

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var a = 3 + 4;
-console.log(a);
+var a = 0;
+var b = false;
 
-function + (a,b){
-  return // add the two #'s
+if (a===b) {
+    console.log('They are equal!');
+}else{
+    console.log('Nope, not equal!');
 }
 
 
@@ -20,7 +22,16 @@ function + (a,b){
 
 
 
-
+// console.log( 1 < 2 < 3);
+// var a = 1,b = '2';
+// console.log(a+b);
+// var a = 3 + 4 * 5;
+//console.log(a);
+// var a = 2, b = 3, c = 4;
+// a = b = c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 //long running function
 // function waitThreeSeconds() {
 //     var ms = 3000 + new Date().getTime();
