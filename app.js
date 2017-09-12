98d407b5196dd84645d871744e27013349faed53
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-11 20:53:46 -0400

    done for the night

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var a = 0;
-var b = false;
-
-if (a===b) {
-    console.log('They are equal!');
-}else{
-    console.log('Nope, not equal!');
+function greet(name) {
+  name = name || '<Your name here>';
+  console.log('Hello ' + name);
 }
 
+greet('Jesse');
+greet();
 
 
 
@@ -18,10 +16,20 @@ if (a===b) {
 
 
 
-
-
-
-
+// var a;
+// //goes to internet and looks for a value
+// a = 'hi';
+// if(a || a === 0){
+//   console.log('Something is there.');
+// }
+// var a = 0;
+// var b = false;
+//
+// if (a===b) {
+//     console.log('They are equal!');
+// }else{
+//     console.log('Nope, not equal!');
+// }
 // console.log( 1 < 2 < 3);
 // var a = 1,b = '2';
 // console.log(a+b);
