7d61e8dc3a6f506f9e6e6c585e58c34664ccc918
Jesse Smith <JesseSmith3rd@outlook.com>
2017-11-07 23:11:05 -0500

    Understanding asynchronous callbacks.

--- practice/javascript.js
+++ practice/javascript.js
@@ -1,3 +1,21 @@
+//Asynchronous: More than one at a time
+//long running function
+function waitThreeSeconds() {
+  var ms = 3000 + new Date().getTime();
+  while (new Date() < ms){}
+  console.log('finished function');
+}
+
+function clickHandler() {
+  console.log('click event!');
+}
+
+//listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
+
 //let allows block scoping.
 
 // a = 2;
